dfa039a7ea05355875e153d7e0c25492d619c634
Steven Anderson <Steven@Mr-Anderson-2.local>
2015-05-26 21:42:18 -0700

    final

--- Fan.h
+++ Fan.h
@@ -1,31 +1,44 @@
 #include <iostream> 
 using namespace std;
-
+///Make class for Fan
 class Fan{
+    
 private:
+    //Set speed private because it needs to be 1,2 or 3.
+    //Set radius private because it needs to be positive.
     int speed;
+    double radius;
     
 public:
+    //no argument constructor for Fan
     Fan () {
         speed=1;
         on=false;
         radius=5;
     }
+    //constructor to create exact Fan
     Fan (int s, bool On, double rad) {
         speed=s;
         on=On;
         radius=rad;
     }
+    //public variable for whether the fans on or off
+      bool on;
     
+    //get and set fuctions for speed
     int getspeed(){return speed;}
-    bool on;
-    double radius;
-    
+   //valid speeds are 1,2, and 3
     void setspeed(int new_speed){
         if (new_speed>0 && new_speed<=3)
             speed=new_speed;
     }
+    //get and set functions for radius
+    //valid radiuses are postive values
+    int getradius(){return radius;}
     
+    void setradius(int new_radius){
+        if (new_radius>0)
+            radius=new_radius;
     
-    
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "Fan.h"
 int main(){
     
-    
+    //create 2 fan objects F1 and F2
+    //use constructor to specify parameters
     Fan F1(3, true,10);
     Fan F2(2, false, 5);
     
-    cout << "Fan 1: " << F1.getspeed()<< "  "<< F1.on<< "  "<< F1.radius<< "  " << endl;
-    cout << "Fan 2: " << F2.getspeed()<< "  "<< F2.on<< "  "<< F2.radius<< "  " << endl;
+    //display the values for each slot for Fan 1
+    //speed, on=1/off=0, radius
+    cout << "Fan 1: " << F1.getspeed()<< "  "<< F1.on<< "  "<< F2.getradius()<< "  " << endl;
+    //display the values for each slot for Fan 2
+      //speed, on=1/off=0, radius
+    cout << "Fan 2: " << F2.getspeed()<< "  "<< F2.on<< "  "<< F2.getradius()<< "  " << endl;
     
-    
+    return 0;
 }
